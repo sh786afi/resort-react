2f6976423b9d82a225aa02d4e707e218da21fed0
shafi akhtar <sh786afi@gmail.com>
2019-05-13 17:32:08 +0530

    first commit

---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class SingleRoom extends Component {
+  render() {
+    return <div>Hello from sigle room page</div>;
+  }
+}
+
+export default SingleRoom;
