3a32f7a1a5c9be4e709044c3452e1a09afb3d93c
shafi akhtar <sh786afi@gmail.com>
2019-05-17 15:10:00 +0530

    final UI

--- src/context.js
+++ src/context.js
@@ -48,13 +48,56 @@ class RoomProvider extends Component {
     return room;
   };
   handleChange = event => {
-    const type = event.target.type;
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
     const name = event.target.name;
-    const value = event.target.value;
-    console.log(type, name, value);
+    this.setState(
+      {
+        [name]: value
+      },
+      this.filterRooms
+    );
   };
   filterRooms = () => {
-    console.log("hello");
+    //filter by type
+    let {
+      rooms,
+      type,
+      capacity,
+      price,
+      minSize,
+      maxSize,
+      breakfast,
+      pets
+    } = this.state;
+    let tempRooms = [...rooms];
+    capacity = parseInt(capacity);
+    if (type !== "all") {
+      tempRooms = tempRooms.filter(room => room.type === type);
+    }
+    //filter by capacity
+    if (capacity !== 1) {
+      tempRooms = tempRooms.filter(room => room.capacity >= capacity);
+    }
+    //filter by price
+    tempRooms = tempRooms.filter(room => room.price <= price);
+
+    //filter by size
+    tempRooms = tempRooms.filter(
+      room => room.size >= minSize && room.size <= maxSize
+    );
+    //filter by breakfast
+    if (breakfast) {
+      tempRooms = tempRooms.filter(room => room.breakfast === true);
+    }
+    //filter by pets
+    if (pets) {
+      tempRooms = tempRooms.filter(room => room.pets === true);
+    }
+    //change state
+    this.setState({
+      sortedRooms: tempRooms
+    });
   };
   render() {
     return (
