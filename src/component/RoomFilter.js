884bc3722b8769bfaa0b4eb91023b29b8b9734f2
shafi akhtar <shafiakhtar@shafis-MacBook-Air.local>
2019-05-17 09:11:39 +0530

    filter

--- src/component/RoomFilter.js
+++ src/component/RoomFilter.js
@@ -1,7 +1,39 @@
 import React from "react";
-
+import { useContext } from "react";
+import { RoomContext } from "../context";
+import Title from "./Title";
 const RoomFilter = () => {
-  return <div>Hello from room filter</div>;
+  const context = useContext(RoomContext);
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets
+  } = context;
+
+  return (
+    <section className="filter-container">
+      <Title title="search rooms" />
+      <form className="filter-form">
+        <div className="form-group">
+          <lalbel htmlFor="type">room type</lalbel>
+          <select
+            name="type"
+            id="type"
+            value={type}
+            className="form-control"
+            onChange={handleChange}
+          />
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default RoomFilter;
--- src/context.js
+++ src/context.js
@@ -20,11 +20,16 @@ class RoomProvider extends Component {
   componentDidMount() {
     let rooms = this.formatData(items);
     let featuredRooms = rooms.filter(room => room.featured === true);
+    let maxPrice = Math.max(...rooms.map(item => item.price));
+    let maxSize = Math.max(...rooms.map(item => item.size));
     this.setState({
       rooms,
       featuredRooms,
       sortedRooms: rooms,
-      loading: false
+      loading: false,
+      price: maxPrice,
+      maxPrice,
+      maxSize
     });
   }
   formatData(items) {
@@ -42,9 +47,24 @@ class RoomProvider extends Component {
     const room = tempRooms.find(room => room.slug === slug);
     return room;
   };
+  handleChange = event => {
+    const type = event.target.type;
+    const name = event.target.name;
+    const value = event.target.value;
+    console.log(type, name, value);
+  };
+  filterRooms = () => {
+    console.log("hello");
+  };
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
+      <RoomContext.Provider
+        value={{
+          ...this.state,
+          getRoom: this.getRoom,
+          handleChange: this.handleChange
+        }}
+      >
         {this.props.children}
       </RoomContext.Provider>
     );
